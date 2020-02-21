cb98ccc9dee463c48de9217dcaa8bfbefe5f3e4a
tangoyankee <miller.tim108@gmail.com>
2020-02-21 14:31:49 -0800

    add black to requirements. lint main.py

--- inventory-allocator/src/main.py
+++ inventory-allocator/src/main.py
@@ -5,26 +5,17 @@ from inventory_allocation import InventoryAllocation
 
 
 def main():
-  warehouses = [
-    { 
-  'name': 'owd', 
-  'inventory': { 'apple': 1, 'banana': 4, 'oranges':10 } 
-  }, {
-   'name': 'dm',
-   'inventory': { 'apple': 7, 'banana': 2, 'oranges':0 } 
-  }, {
-    'name': 'empty',
-    'inventory': {}
-  }, {
-    'name': 'exotic',
-    'inventory': {'apple': 1, 'avocado': 4, 'banana': 6}
-  }
-]
-  order = {'apple': 4, 'banana': 13}
-  inv_allo = InventoryAllocation()
-  warehouse_fulfillment = inv_allo.get_warehouse_fulfillment(order, warehouses)
-  return(warehouse_fulfillment)
+    warehouses = [
+        {"name": "owd", "inventory": {"apple": 1, "banana": 4, "oranges": 10}},
+        {"name": "dm", "inventory": {"apple": 7, "banana": 2, "oranges": 0}},
+        {"name": "empty", "inventory": {}},
+        {"name": "exotic", "inventory": {"apple": 1, "avocado": 4, "banana": 6}},
+    ]
+    order = {"apple": 4, "banana": 13}
+    inv_allo = InventoryAllocation()
+    warehouse_fulfillment = inv_allo.get_warehouse_fulfillment(order, warehouses)
+    return warehouse_fulfillment
+
 
 if __name__ == "__main__":
-  print (main())
-  
+    print(main())
